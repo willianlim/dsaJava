c9e5b727686023076e7ae384aaea110e0d1a1356
willianlim <willianrosendodelima@outlook.com>
2022-11-21 06:50:15 -0300

    reverse an array in java

--- src/array/ArrayTest.java
+++ src/array/ArrayTest.java
@@ -3,12 +3,21 @@ package array;
 public class ArrayTest {
 
     public static void main(String[] args) {
+        System.out.println("=== 1º ===");
         ArrayUtil arrayUtil = new ArrayUtil();
         arrayUtil.arrayDemo();
         System.out.println();
 
+        System.out.println("=== 2º ===");
         int[] result = arrayUtil.removeEven(new int[] {3, 2, 4, 7, 10, 6, 5});
         arrayUtil.printArray(result);
+        System.out.println();
+
+        System.out.println("=== 3º ===");
+        int[] numbers = {2, 11, 5, 10, 7, 8};
+        arrayUtil.printArray(numbers);
+        arrayUtil.reverse(numbers, 0, numbers.length - 1);
+        arrayUtil.printArray(numbers);
     }
 
 }
--- src/array/ArrayUtil.java
+++ src/array/ArrayUtil.java
@@ -44,4 +44,14 @@ public class ArrayUtil {
         }
         return (result);
     }
+
+    public void reverse(int[] numbers, int start, int end) {
+        while (start < end) {
+            int temp = numbers[start];
+            numbers[start] = numbers[end];
+            numbers[end] = temp;
+            start++;
+            end--;
+        }
+    }
 }
