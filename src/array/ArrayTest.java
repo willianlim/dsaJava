e86b4875570f6de03e78058d4b727c42a65fb65b
willianlim <willianrosendodelima@outlook.com>
2022-11-21 06:32:20 -0300

    test array

---
+++ src/array/ArrayTest.java
@@ -0,0 +1,10 @@
+package array;
+
+public class ArrayTest {
+
+    public static void main(String[] args) {
+        ArrayUtil arrayUtil = new ArrayUtil();
+        arrayUtil.arrayDemo();
+    }
+
+}
